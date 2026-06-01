4990834d7e444bf4ffc40667352f51eecc650e18
Mateus Navarro Bella Cruz <mateusbcruz@hotmail.com>
2026-06-01 20:06:10 -0300

    Start da infraestrutura

--- infrastructure/scripts/startinfra.sh
+++ infrastructure/scripts/startinfra.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 cd ..
 cd docker
-docker compose -f docker-compose.yml up -d --wait --profile infraestrutura
+docker compose --profile infraestrutura -f docker-compose.yml up -d --wait
 echo "Os containers estão rodando"
 cd ..
 cd scripts
